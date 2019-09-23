12a7f506ec4cbaa03e413767e099df6939701af3
Julie Ann Dela Pena <delapena.julieann@hotmail.com>
2019-09-22 20:20:36 -0700

    updated folders

---
+++ programmiz/1-Introduction/examples-introduction/sample01.c
@@ -0,0 +1,9 @@
+// c program that prints hello world..
+
+#include <stdio.h>
+int main(void)
+{
+  printf("Welcome to C Programing Language!\n");
+
+  return 0;
+}
---
+++ programmiz/1-Introduction/examples-introduction/sample02.c
@@ -0,0 +1,14 @@
+// c program to print an integer insert by the user.
+
+#include<stdio.h>
+int main(void)
+{
+  int input1;
+  printf("Enter an integer: \n");
+  scanf("%d", &input1);
+
+  printf("\n\n");
+  printf("The integer you input is %d", input1);
+
+  return 0;
+}
