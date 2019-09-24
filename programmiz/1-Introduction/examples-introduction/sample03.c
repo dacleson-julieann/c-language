44787fa6f421898c9ea7b2cd3aa0871d796e1222
Julie Ann Dela Pena <delapena.julieann@hotmail.com>
2019-09-24 11:15:58 -0700

    Create sample03.c

---
+++ programmiz/1-Introduction/examples-introduction/sample03.c
@@ -0,0 +1,16 @@
+// c program to add two integers entered by the user.
+
+#include<studio.h>
+int main(void)
+{
+  int input1, input2;
+  
+  printf("Enter the 1st integer: \n");
+  scanf("%d", &input1);
+  printf("Enter the 2nd integer: \n");
+  scanf("%d", &input2);
+  
+  printf("The sum of %d and %d is %d\n", input1, input2, input1 + input2);
+  
+  return 0:
+}
